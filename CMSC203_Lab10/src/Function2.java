89feee6edc68cb1e405b978c5be5104f07115eb4
kmarsh423 <55006092+kmarsh423@users.noreply.github.com>
2020-04-11 13:11:08 -0400

    Add files via upload

---
+++ CMSC203_Lab10/src/Function2.java
@@ -0,0 +1,13 @@
+
+public class Function2 extends Function {
+
+	@Override
+	public double fnValue(double x) {
+		return Math.sin(x);
+	}
+	
+	public String toString() {
+		return "sin(x)";
+	}
+
+}
---
+++ CMSC203_Lab10/src/Function3.java
@@ -0,0 +1,13 @@
+
+public class Function3 extends Function {
+
+	@Override
+	public double fnValue(double x) {
+		return (8*x-Math.sqrt(x))/(Math.pow(x,3) - (7*Math.pow(x,2)) + 15*x - 9);
+	}
+	
+	public String toString () {
+		return "(8*x-sqrt(x))/x^3 - 7*x^2 + 15*x - 9";
+	}
+
+}
